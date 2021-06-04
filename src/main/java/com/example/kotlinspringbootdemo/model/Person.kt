d80adf3d6cf9d4fa68f665e2157707379bb6713a
Davis Mohar <davis.mohar@missionlane.com>
2021-06-04 16:04:42 -0400

    Adding JsonProperty annotations to Person

--- src/main/java/com/example/kotlinspringbootdemo/model/Person.kt
+++ src/main/java/com/example/kotlinspringbootdemo/model/Person.kt
@@ -1,9 +1,11 @@
 package com.example.kotlinspringbootdemo.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class Person(
-    var firstName: String,
-    var lastName: String,
-    var age: Int
+    @JsonProperty("firstName") var firstName : String,
+    @JsonProperty("lastName") var lastName: String,
+    @JsonProperty("age") var age: Int
 ) {
     override fun toString(): String {
         return "$firstName $lastName"
